10d7a58c3b76f918e961568c2c9dcb869298d018
Mostafa El Sharkawy <elsharkawy.mostafa20@gmail.com>
2025-09-11 21:40:52 +0300

    Lab 1 and 2

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            Console.WriteLine("Task 1");
+            Console.Write("Enter a character: ");
+            string inputChar = Console.ReadLine();
+            int asciiCode = (int)inputChar[0];
+            Console.WriteLine($"The ASCII Code of {inputChar} is {asciiCode}");
+            #endregion
+
+            #region Task2
+            Console.WriteLine("\nTask 2");
+            Console.Write("Enter an ASCII Code: ");
+            int asciiCode2 = Convert.ToInt32(Console.ReadLine());
+            char outputChar2 = (char)asciiCode2;
+            Console.WriteLine($"The Char of {asciiCode2} is {outputChar2}");
+            #endregion
+
+            #region Task3
+            Console.WriteLine("\nTask 3");
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number % 2 == 0 ? "Even" : "Odd");
+            #endregion
+
+            #region Task4
+            Console.WriteLine("\nTask 4");
+            Console.Write("Enter number 1: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Sum = {number1 + number2}");
+            Console.WriteLine($"Subraction = {number1 - number2}");
+            Console.WriteLine($"Multiplication = {number1 * number2}");
+            #endregion
+
+            #region Task5
+            Console.WriteLine("\nTask 5");
+            Console.Write("Enter a number: ");
+            int degree = Convert.ToInt32(Console.ReadLine());
+            if(degree >= 90)
+                Console.WriteLine("A");
+            else if(degree >= 80)
+                Console.WriteLine("B");
+            else if(degree >= 70)
+                Console.WriteLine("C");
+            else if(degree >= 60)
+                Console.WriteLine("D");
+            else
+                Console.WriteLine("F");
+            #endregion
+
+            #region Task6
+            Console.WriteLine("\nTask 6");
+            Console.Write("Enter a number: ");
+            int multiplicationTableNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Multiplication Table of {multiplicationTableNumber}:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{multiplicationTableNumber} x {i} = {multiplicationTableNumber * i}");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lab2
+{
+    internal class Program
+    {
+        struct Time
+        {
+            public int hours;
+            public int minutes;
+            public int seconds;
+
+            public void print()
+            {
+                Console.WriteLine($"{hours}H:{minutes}M:{seconds}S");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            #region Task1
+            Console.WriteLine("Task 1:");
+            Console.Write("Enter the number of students: ");
+            int numberOfStudents = int.Parse(Console.ReadLine());
+            string[] studentsNames = new string[numberOfStudents];
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                Console.Write($"Enter the name of student {i + 1}: ");
+                studentsNames[i] = Console.ReadLine();
+            }
+            foreach (string name in studentsNames)
+            {
+                Console.WriteLine(name);
+            }
+            #endregion
+
+            #region Task2
+            Console.WriteLine("\nTask 2:");
+            Console.Write("Enter the number of Tracks: ");
+            int numberOfTracks = int.Parse(Console.ReadLine());
+            int[][] trackStudentsAges = new int[numberOfTracks][];
+            for (int i = 0; i < numberOfTracks; i++)
+            {
+                Console.Write($"Enter the number of students in Track {i + 1}: ");
+                int numberOfStudentsInTrack = int.Parse(Console.ReadLine());
+                trackStudentsAges[i] = new int[numberOfStudentsInTrack];
+                for (int j = 0; j < numberOfStudentsInTrack; j++)
+                {
+                    Console.Write($"Enter the age of student {j + 1} in Track {i + 1}: ");
+                    trackStudentsAges[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            for (int i = 0; i < numberOfTracks; i++)
+            {
+                Console.WriteLine($"Ages of students in Track {i + 1}:");
+                foreach (int age in trackStudentsAges[i])
+                {
+                    Console.WriteLine(age);
+                }
+                Console.WriteLine($"Avg Age = {trackStudentsAges[i].Average()}");
+            }
+
+            #endregion
+            #region Task3
+            Console.WriteLine("\nTask 3:");
+            Time time;
+            time.hours = 1;
+            time.minutes = 30;
+            time.seconds = 45;
+            time.print();
+            #endregion
+        }
+    }
+}
